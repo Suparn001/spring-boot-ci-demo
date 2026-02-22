f3c8df24f99ce386c2c5c1e75cf20b24f381d330
Suparn001 <suparngautam@gmail.com>
2026-02-22 08:12:06 +0530

    update

--- src/main/java/com/example/aws_deployment_ci_cd/HelloController.java
+++ src/main/java/com/example/aws_deployment_ci_cd/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String Hello(){
-        return "Hello World";
+        return "Hello from CI/CD Pipeline...";
     }
 }
